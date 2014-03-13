8bd0c347c851b07875a23f70c3628cceedad4d31
Syeoryn <drewcuth1@gmail.com>
2014-03-13 01:13:44 -0500

    _.reduce added

--- src/underbar.js
+++ src/underbar.js
@@ -144,6 +144,14 @@ var _ = { };
   //     return total + number;
   //   }, 0); // should be 6
   _.reduce = function(collection, iterator, accumulator) {
+	if(typeof accumulator == 'undefined') {
+		accumulator = collection[0];
+		var collection = collection.slice(1);
+	}
+	_.each(collection,function(num){
+		accumulator += num
+	});
+	return accumulator;
   };
 
   // Determine if the array or object contains a given value (using `===`).
