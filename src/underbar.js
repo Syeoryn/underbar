5e086dc659f3f7f63ed22df29e638af4629e14dc
Syeoryn <drewcuth1@gmail.com>
2014-03-14 01:18:42 -0500

    _.memoize added

--- src/underbar.js
+++ src/underbar.js
@@ -264,6 +264,14 @@ var _ = { };
   // already computed the result for the given argument and return that value
   // instead if possible.
   _.memoize = function(func) {
+	var results = {};
+	
+	return function(){
+		var key = _.identity.apply(this,arguments);
+		if(!(key in results)) results[key] = func.apply(this,arguments)
+		
+		return results[key];
+	}
   };
 
   // Delays a function for the given number of milliseconds, and then calls
